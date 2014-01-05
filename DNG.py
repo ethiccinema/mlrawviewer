9a205f4624eea37c198ba7a4b0a724ce3eafe056
Andrew Baldwin <baldand-bb@dehabit.info>
2014-01-05 23:01:32 +0200

    Writing out of read/parsed DNGs with all tags

--- DNG.py
+++ DNG.py
@@ -54,6 +54,7 @@ class Tag:
     BitsPerSample = (258,Type.Short)
     Compression = (259,Type.Short)
     PhotometricInterpretation = (262,Type.Short)
+    FillOrder = (266,Type.Short)
     ImageDescription = (270,Type.Ascii)
     Make = (271,Type.Ascii)
     Model = (272,Type.Ascii)
@@ -70,6 +71,7 @@ class Tag:
     DateTime = (306,Type.Ascii)
     Artist = (315,Type.Ascii)
     SubIFD = (330,Type.Long)
+    XMP_Metadata = (700,Type.Undefined)
     CFARepeatPatternDim = (33421,Type.Short)
     CFAPattern = (33422,Type.Byte)
     Copyright = (33432,Type.Ascii)
@@ -96,22 +98,45 @@ class Tag:
     DNGVersion = (50706,Type.Byte)
     DNGBackwardVersion = (50707,Type.Byte)
     UniqueCameraModel = (50708,Type.Ascii)
+    CFAPlaneColor = (50710,Type.Byte)
+    CFALayout = (50711,Type.Short)
+    BlackLevelRepeatDim = (50713,Type.Short)
     BlackLevel = (50714,Type.Long)
     WhiteLevel = (50717,Type.Long)
+    DefaultScale = (50718,Type.Rational)
     DefaultCropOrigin = (50719,Type.Long)
     DefaultCropSize = (50720,Type.Long)
     ColorMatrix1 = (50721,Type.Srational)
+    ColorMatrix2 = (50722,Type.Srational)
+    CameraCalibration1 = (50723,Type.Srational)
+    CameraCalibration2 = (50724,Type.Srational)
     AnalogBalance = (50727,Type.Rational)
     AsShotNeutral = (50728,Type.Rational)
     BaselineExposure = (50730,Type.Srational)
     BaselineNoise = (50731,Type.Rational)
     BaselineSharpness = (50732,Type.Rational)
+    BayerGreenSplit = (50733,Type.Long)
     LinearResponseLimit = (50734,Type.Rational)
+    AntiAliasStrength = (50738,Type.Rational)
+    ShadowScale = (50739,Type.Rational)
+    DNGPrivateData = (50740,Type.Byte)
+    MakerNoteSafety = (50741,Type.Short)
     CalibrationIlluminant1 = (50778,Type.Short)
     CalibrationIlluminant2 = (50779,Type.Short)
+    BestQualityScale = (50780,Type.Rational)
+    RawDataUniqueID = (50781,Type.Byte)
     ActiveArea = (50829,Type.Long)
+    CameraCalibrationSignature = (50931,Type.Ascii)
+    ProfileCalibrationSignature = (50932,Type.Ascii)
+    NoiseReductionApplied = (50935,Type.Rational)
+    ProfileHueSatMapDims = (50937,Type.Long)
+    ProfileHueSatMapData1 = (50938,Type.Float)
+    ProfileHueSatMapData2 = (50939,Type.Float)
+    NoiseProfile = (51041,Type.Double)
     FrameRate = (51044,Type.Srational)
     OpcodeList1 = (51008,Type.Undefined)
+    OpcodeList2 = (51009,Type.Undefined)
+    ReelName = (51081,Type.Ascii)
 
 IfdNames = [n for n in dir(Tag) if n!="__doc__" and n!="__module__"]
 IfdValues = [getattr(Tag,n) for n in IfdNames]
@@ -121,7 +146,7 @@ IfdLookup = dict(zip(IfdIdentifiers,IfdNames))
 
 class DNG(object):
 
-    def __init__(self,log=False,parse=True): 
+    def __init__(self,log=False,parse=True):
         self.log = log
         self.autoparse = parse
         self.df = None # File
@@ -145,7 +170,7 @@ class DNG(object):
 
     def readFileIn(self,filename):
         """
-        Read entire file from file system 
+        Read entire file from file system
         Only 1 large file read operation
         """
         df = file(filename,'rb')
@@ -153,6 +178,102 @@ class DNG(object):
         df.close()
         if self.log: print "DNG file",filename,"length",len(self.dd)
         if self.autoparse: self.parse()
+
+    def readAll(self):
+        """
+        Parse all strips
+            """
+        total = 0
+        if self.FULL_IFD:
+            s = self.FULL_IFD.strips()
+            for st in s:
+                total += len(st)
+        if self.THUMB_IFD:
+            s = self.THUMB_IFD.strips()
+            for st in s:
+                total += len(st)
+        self.stripTotal = total
+
+    def align(self,val):
+        return (val+1)&0xFFFFFFE
+    def writeTag(self,tag,buf,off,extraDataOff):
+        header,extra = self.encodeTagData(tag,extraDataOff)
+        buf[off:off+12] = header
+        if extra:
+            buf[extraDataOff:extraDataOff+len(extra)] = extra
+            return len(extra)
+        else:
+            return 0
+    def writeIfd(self,buf,off,ifd):
+        entryMap = [] # Used to patch IFD pointers later
+        extraDataOff = self.align(off+2+len(ifd.entries)*12+4) # Each entry consumes 12 bytes. Extra data must be aligned to word
+        buf[off:off+2] = struct.pack(self.bo+"H",len(ifd.entries))
+        off += 2
+        for e in ifd.entries:
+            extraInc = self.writeTag(e,buf,off,extraDataOff)
+            entryMap.append((e[0],off,extraDataOff))
+            extraDataOff += extraInc
+            off += 12
+        buf[off:off+4] = struct.pack(self.bo+"I",0) # Don't support multiple chained IFDs.
+        ifd.entryMap = entryMap
+        return extraDataOff
+
+    def writeFile(self,filename):
+        """
+        Write out a new (Cinema)DNG file with the current IFD settings
+        This supports copying/modifying DNGs even with unknown tags
+        since we should have everything we need when loading from existing file
+        """
+        if self.log: print "Writing to",filename
+        # Prepare everything in an array
+        dl = 1000000
+        b = bytearray(self.stripTotal + 1000000) # Size of the data + 1M to be sure
+        # Prepare file header
+        if self.bo == "<":
+            byteOrder = "II"
+        else:
+            byteOrder = "MM"
+        b[0:8] = struct.pack(self.bo+"2sHI",byteOrder,42,8) # Will begin immediately after the header
+        off = 8
+
+        # Write first IFD block
+        ifd0len = self.writeIfd(b,off,self.ifds[0])
+        off += ifd0len
+        # Write any subIFD
+        subifd = None
+        exififd = None
+        for tag,offset,extraOffset in self.ifds[0].entryMap:
+            if tag == Tag.SubIFD[0]:
+                subifd = (offset,extraOffset)
+            if tag == Tag.EXIF_IFD[0]:
+                exififd = (offset,extraOffset)
+        for sindex,sifd in enumerate(self.ifds[0].subIFDs):
+            # Fix up link to this subIFD in IFD0
+            if len(self.ifds[0].subIFDs)==1:
+                b[subifd[0]+8:subifd[0]+12] = struct.pack(self.bo+"I",off)
+            else:
+                b[subifd[1]+4*sindex:subifd[1]+4*sindex+4] = struct.pack(self.bo+"I",off)
+            off += self.writeIfd(b,off,sifd)
+
+        # Write EXIF IFD
+        if self.ifds[0].EXIF_IFD:
+            # Fix up link to EXIF_IDF in IFD0
+            b[exififd[0]+8:exififd[0]+12] = struct.pack(self.bo+"I",off)
+            off += self.writeIfd(b,off,self.ifds[0].EXIF_IFD)
+
+        # Write first strips
+        off += self.ifds[0].writeStrips(b,off)
+
+        # Write subifd strips
+        for sindex,sifd in enumerate(self.ifds[0].subIFDs):
+            off += sifd.writeStrips(b,off)
+
+        # Done with preparation
+
+        # Write the whole file to disk
+        outfile = file(filename,'wb')
+        outfile.write(b[:off])
+        outfile.close()
 
     def parse(self):
         self.ifds = []
@@ -189,7 +310,7 @@ class DNG(object):
         if byteOrder == 0x4949: # Little endian
             if self.log: print "Little endian"
             self.bo = "<"
-        elif byteOrder == 0x4D4d: # Big endian
+        elif byteOrder == 0x4D4D: # Big endian
             if self.log: print "Big endian"
             self.bo = ">"
         valid,firstIfdOffset = struct.unpack(self.bo+"HI",h[2:])
@@ -209,9 +330,11 @@ class DNG(object):
             self.length = None
             self.RowsPerStrip = None
             self.PlanarConfiguration = None
-        def subFileType(self):  
+            self.BitsPerSample = None
+            self._strips = None
+        def subFileType(self):
             t = self.subFileType
-            if t&1: 
+            if t&1:
                 if self.log: print "ReducedResolution"
             if t&1==0:
                 if self.log: print "FullResolution"
@@ -225,6 +348,8 @@ class DNG(object):
         def isThumb(self):
             return self.subFileType&1==1
         def strips(self):
+            if self._strips: return self._strips
+            # Load the strips
             if not self.RowsPerStrip: raise IOError
             if not self.length: raise IOError
             if not self.PlanarConfiguration: raise IOError
@@ -234,17 +359,50 @@ class DNG(object):
             StripsPerImage = self.RowsPerStrip * self.length
             StripByteCounts = self.tags[Tag.StripByteCounts[0]][3]
             StripOffsets = self.tags[Tag.StripOffsets[0]][3]
-            s = []
+            self._strips = []
             for byteCount,offset in zip(StripByteCounts,StripOffsets):
                 strip = self.dng.readFrom(offset,byteCount)
-                s.append(strip)
-            return s
+                self._strips.append(strip)
+            return self._strips
         def stripsCombined(self):
             s = self.strips()
             if len(s)==1:
                 return s[0]
             else:
                 return ''.join(s)
+        def writeStrips(self,buf,offset):
+            if not self._strips: raise IOError
+            if not self.BitsPerSample: raise IOError
+            if not self.RowsPerStrip: raise IOError
+            for tag,toffset,extraOffset in self.entryMap:
+                if tag == Tag.StripByteCounts[0]:
+                    sbc = (toffset,extraOffset)
+                if tag == Tag.StripOffsets[0]:
+                    so = (toffset,extraOffset)
+            StripsPerImage = self.RowsPerStrip * self.length
+            BytesPerRow = self.width * sum(self.BitsPerSample) / 8
+            BytesPerStrip = BytesPerRow * self.RowsPerStrip
+            # Break up self._strips according to these settings
+            left = len(self._strips)
+            stripoff = 0
+            stripNum = 0
+            for s in self._strips:
+                stripLen = len(s)
+                buf[offset:offset+stripLen] = s
+                if len(self._strips)==1:
+                    i = 0
+                    o = 8
+                else:
+                    i = 1
+                    o = stripNum*4
+                buf[so[i]+o:so[i]+o+4] = struct.pack(self.dng.bo+"I",offset)
+                buf[sbc[i]+o:sbc[i]+o+4] = struct.pack(self.dng.bo+"I",stripLen)
+                offset += stripLen
+                left -= stripLen
+                stripoff += stripLen
+                stripNum += 1
+            return offset
+
 
     def readIfd(self,offset):
         count = struct.unpack(self.bo+"H",self.readFrom(offset,2))[0]
@@ -260,37 +418,79 @@ class DNG(object):
         nextOffset = struct.unpack(self.bo+"I",ifdEntryData[-4:])[0]
         return ifd,nextOffset
 
+    def encodeTagData(self,tag,extraOffset):
+        itag,itype,icount,data = tag
+        if itype==Type.Invalid[0]:
+            raise IOError
+        elif itype==Type.Byte[0]:
+            values = struct.pack(self.bo+"%dB"%icount,*data[:icount])
+        elif itype==Type.Ascii[0]:
+            values = struct.pack(self.bo+"%ds"%icount,data[:icount])
+        elif itype==Type.Short[0]:
+            values = struct.pack(self.bo+"%dH"%icount,*data[:icount])
+        elif itype==Type.Long[0]:
+            values = struct.pack(self.bo+"%dI"%icount,*data[:icount])
+        elif itype==Type.Rational[0]:
+            flattened = [e for nd in data[:icount] for e in nd]
+            values = struct.pack(self.bo+"%dI"%(icount*2),*flattened)
+        elif itype==Type.Sbyte[0]:
+            values = struct.pack(self.bo+"%db"%icount,*data[:icount])
+        elif itype==Type.Undefined[0]:
+            values = data[:icount]
+        elif itype==Type.Sshort[0]:
+            values = struct.unpack(self.bo+"%dh"%icount,*data[:icount])
+        elif itype==Type.Slong[0]:
+            values = struct.pack(self.bo+"%di"%icount,*data[:icount])
+        elif itype==Type.Srational[0]:
+            flattened = [e for nd in data[:icount] for e in nd]
+            values = struct.pack(self.bo+"%di"%(icount*2),*flattened)
+        elif itype==Type.Float[0]:
+            values = struct.pack(self.bo+"%df"%icount,*data[:icount])
+        elif itype==Type.Double[0]:
+            values = struct.pack(self.bo+"%dd"%icount,*data[:icount])
+        elif itype==Type.IFD[0]:
+            values = struct.pack(self.bo+"%dI"%icount,*data[:icount])
+        if len(values)>4:
+            header = struct.pack(self.bo+"HHII",itag,itype,icount,extraOffset)
+            extra = values
+        else: # Value embedded
+            # Pad values to 4 bytes
+            values += chr(0)*(4-len(values))
+            header = struct.pack(self.bo+"HHI4s",itag,itype,icount,values)
+            extra = None
+        return header,extra
+
     def decodeTagData(self,itype,icount,data):
         if itype==Type.Invalid[0]:
             raise IOError
         elif itype==Type.Byte[0]:
-            return struct.unpack("<"+"%dB"%icount,data[:icount])
+            return struct.unpack(self.bo+"%dB"%icount,data[:icount])
         elif itype==Type.Ascii[0]:
-            return struct.unpack("<"+"%ds"%icount,data[:icount])[0]
+            return struct.unpack(self.bo+"%ds"%icount,data[:icount])[0]
         elif itype==Type.Short[0]:
-            return struct.unpack("<"+"%dH"%icount,data[:icount*2])
+            return struct.unpack(self.bo+"%dH"%icount,data[:icount*2])
         elif itype==Type.Long[0]:
-            return struct.unpack("<"+"%dI"%icount,data[:icount*4])
+            return struct.unpack(self.bo+"%dI"%icount,data[:icount*4])
         elif itype==Type.Rational[0]:
-            rationals = struct.unpack("<"+"%dI"%(icount*2),data[:icount*8])
+            rationals = struct.unpack(self.bo+"%dI"%(icount*2),data[:icount*8])
             return tuple((rationals[i*2],rationals[i*2+1]) for i in range(icount))
         elif itype==Type.Sbyte[0]:
-            return struct.unpack("<"+"%db"%icount,data[:icount])
+            return struct.unpack(self.bo+"%db"%icount,data[:icount])
         elif itype==Type.Undefined[0]:
             return data[:icount]
         elif itype==Type.Sshort[0]:
-            return struct.unpack("<"+"%dh"%icount,data[:icount*2])
+            return struct.unpack(self.bo+"%dh"%icount,data[:icount*2])
         elif itype==Type.Slong[0]:
-            return struct.unpack("<"+"%di"%icount,data[:icount*4])
+            return struct.unpack(self.bo+"%di"%icount,data[:icount*4])
         elif itype==Type.Srational[0]:
-            rationals = struct.unpack("<"+"%di"%(icount*2),data[:icount*8])
+            rationals = struct.unpack(self.bo+"%di"%(icount*2),data[:icount*8])
             return tuple((rationals[i*2],rationals[i*2+1]) for i in range(icount))
         elif itype==Type.Float[0]:
-            return struct.unpack("<"+"%df"%icount,data[:icount*4])
+            return struct.unpack(self.bo+"%df"%icount,data[:icount*4])
         elif itype==Type.Double[0]:
-            return struct.unpack("<"+"%dd"%icount,data[:icount*8])
+            return struct.unpack(self.bo+"%dd"%icount,data[:icount*8])
         elif itype==Type.IFD[0]:
-            return struct.unpack("<"+"%dI"%icount,data[:icount*4])
+            return struct.unpack(self.bo+"%dI"%icount,data[:icount*4])
 
     def readIfdEntry(self,ifd,ifdEntry,data):
         itag,itype,icount,ival = ifdEntry
@@ -300,7 +500,7 @@ class DNG(object):
             data = self.readFrom(ival,totalSize)
 
         values = self.decodeTagData(itype,icount,data)
-           
+
         IfdName = IfdLookup.get(itag,"Unknown")
         if itag==Tag.NewSubfileType[0]:
             ifd.subFileType = ival
@@ -310,10 +510,10 @@ class DNG(object):
                 self.THUMB_IFD = ifd
         elif itag==Tag.ImageWidth[0]:
             ifd.width = ival
-            if self.log: print "Width:",ifd.width 
+            if self.log: print "Width:",ifd.width
         elif itag==Tag.ImageLength[0]:
             ifd.length = ival
-            if self.log: print "Length:",ifd.length 
+            if self.log: print "Length:",ifd.length
         elif itag==Tag.SubIFD[0]:
             if self.log: print "Reading SubIFDs"
             for suboffset in values:
@@ -330,13 +530,15 @@ class DNG(object):
             ifd.RowsPerStrip = ival
         elif itag==Tag.PlanarConfiguration[0]:
             ifd.PlanarConfiguration = ival
-        
+        elif itag==Tag.BitsPerSample[0]:
+            ifd.BitsPerSample = values
+
         ifdEntry = (itag,itype,icount,values)
-        if self.log: print IfdName,ifdEntry 
-        return ifdEntry 
+        if self.log: print IfdName,ifdEntry
+        return ifdEntry
 
     def write(self,filename):
-        pass 
+        pass
 
 def testDngDirRead(path):
     dngs = [i for i in os.listdir(sys.argv[1]) if i.lower().endswith(".dng")]
@@ -375,9 +577,18 @@ def testReadDng(filename):
         print len(full)
     d.close()
 
+def testCopyDng(dngin,dngout):
+    d = DNG(log=True,parse=True)
+    d.readFileIn(dngin)
+    d.readAll()
+    d.writeFile(dngout)
+    d.close()
+
 if __name__ == '__main__':
     if os.path.isdir(sys.argv[1]):
         testDngDirRead(sys.argv[1])
-    else:
+    elif len(sys.argv)==2:
         testReadDng(sys.argv[1])
+    elif len(sys.argv)==3:
+        testCopyDng(sys.argv[1],sys.argv[2])
 
