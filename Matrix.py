325e6accbf03d8767be9b07690c4fa48cfbbce0e
Andrew Baldwin <baldand-bb@dehabit.info>
2014-02-25 18:13:55 +0200

    Remove dependency on pyrr
    Matrix4x4 functionality just depends (again) on numpy now

--- Matrix.py
+++ Matrix.py
@@ -1,56 +1,61 @@
 import numpy as np
 import math
 
-import pyrr.matrix44
-from pyrr.matrix44 import *
+def multiply(ma,mb):
+    return np.dot(ma,mb)
+
+def identity():
+    return np.matrix(np.identity( 4, dtype=np.float32))
 
 class Matrix4x4(object):
     def __init__(self):
-        md = np.zeros(shape=(4,4),dtype=np.float32)
-        m = np.matrix(md)
-        self.m = m
-        self.width = 2.0
-        self.height = 2.0   
-        self.identity()
+        self.m = identity()
     def copy(self,other):
         self.m = other.m
-        self.width = other.width
-        self.height = other.height
     def reset(self):
         self.identity()
     def identity(self):
-        self.m[:,:] = 0.
-        self.m[0,0]=1.
-        self.m[1,1]=1.
-        self.m[2,2]=1.
-        self.m[3,3]=1.
-        self.width = 2.0
-        self.height = 2.0   
+        self.m = identity()
     def viewport(self,width,height):
-        if hasattr(pyrr.matrix44,"create_orthogonal_view_matrix"):
-             proj = create_orthogonal_view_matrix(0.0,width,0.0,height,0.0,1.0)
-        else:
-             proj = create_orthogonal_projection_matrix(0.0,width,0.0,height,0.0,1.0)
+        proj = identity()
+        proj[0,0] = 2.0/width
+        proj[1,1] = -2.0/height
+        proj[2,2] = -2.0
         self.m = multiply(proj,self.m)
-        self.width = float(width)
-        self.height = float(height)
     def scale(self,factor):
-        scale = create_from_scale([factor,factor,1.0])
+        scale = identity() 
+        scale[0,0] = factor
+        scale[1,1] = factor
         self.m = multiply(scale,self.m)
     def scale2d(self,width,height):
-        scale = create_from_scale([width,height,1.0])
+        scale = identity() 
+        scale[0,0] = width
+        scale[1,1] = height
         self.m = multiply(scale,self.m)
     def translate(self,x,y):
-        trans = create_from_translation([x,y,0.0])
+        trans = identity() 
+        trans[3,:2] = [x,y]
         self.m = multiply(trans,self.m)
     def rotation(self,angle):
-        rot = create_from_z_rotation(angle)
+        rot = identity() 
+        ca = math.cos(angle)
+        sa = math.sin(angle)
+        rot[:2,:2] = np.array([[ca,-sa],[sa,ca]])
         self.m = multiply(rot,self.m)
     def mult(self,matrix):
         self.m *= matrix.m 
     def multvec(self,x,y):
-        return apply_to_vector(self.m,(x,y,0.0))
+        v = np.array((x,y,0.0,1.0))
+        r = np.empty(shape=(4,))
+        r[:] = np.dot(v,self.m)[0,:]
+        if r[3] != 0.0: r = r/r[3]
+        return r[:3]
     def multveci(self,x,y):
-        return apply_to_vector(inverse(self.m),(x,y,0.0))
+        m = self.m.getI()
+        v = np.array([x,y,0.0,1.0])
+        r = np.empty(shape=(4,))
+        r[:] = np.dot(v,m)[0,:]
+        if r[3] != 0.0: r = r/r[3]
+        return r[:3]
     
 
