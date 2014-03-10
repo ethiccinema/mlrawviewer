2d162489a8e30dc842d58e2e1a25eadb576e0da0
Andrew Baldwin <baldand-bb@dehabit.info>
2014-03-10 10:54:00 +0200

    Make ExportQueue daemon thread so doesn't block shutdown

--- mlrawviewer.py
+++ mlrawviewer.py
@@ -35,7 +35,7 @@ import tkFileDialog
 
 from Config import Config
 
-config = Config(version=(1,1,1))
+config = Config(version=(1,1,2))
 
 programpath = os.path.abspath(os.path.split(sys.argv[0])[0])
 if getattr(sys,'frozen',False):
