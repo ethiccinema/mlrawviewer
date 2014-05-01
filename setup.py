0bd1052e57bce11101e3ce4ee7f9d9f2133eb7c7
Kevin Deldycke <kevin@deldycke.com>
2014-05-01 18:46:24 +0000

    Version number must be a string to not trigger the following issue on package install:
    ```bash
    mlrawviewer(master@15a843f) $ sudo python setup.py install
    running install
    running build
    running build_ext
    running install_lib
    running install_egg_info
    Traceback (most recent call last):
      File "setup.py", line 6, in <module>
        setup ( name = "bitunpack", version = 1.0, description = "Fast bit unpacking functions", ext_modules = [module1])
      File "/usr/lib/python2.7/distutils/core.py", line 151, in setup
        dist.run_commands()
      File "/usr/lib/python2.7/distutils/dist.py", line 953, in run_commands
        self.run_command(cmd)
      File "/usr/lib/python2.7/distutils/dist.py", line 972, in run_command
        cmd_obj.run()
      File "/usr/lib/python2.7/distutils/command/install.py", line 613, in run
        self.run_command(cmd_name)
      File "/usr/lib/python2.7/distutils/cmd.py", line 326, in run_command
        self.distribution.run_command(command)
      File "/usr/lib/python2.7/distutils/dist.py", line 971, in run_command
        cmd_obj.ensure_finalized()
      File "/usr/lib/python2.7/distutils/cmd.py", line 109, in ensure_finalized
        self.finalize_options()
      File "/usr/lib/python2.7/distutils/command/install_egg_info.py", line 46, in finalize_options
        to_filename(safe_version(self.distribution.get_version()))
      File "/usr/lib/python2.7/distutils/command/install_egg_info.py", line 88, in safe_version
        version = version.replace(' ','.')
    AttributeError: 'float' object has no attribute 'replace'
    ```

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup, Extension
 module1 = Extension('bitunpack', sources = ["bitunpack.c","amaze_demosaic_RT.c"],  extra_compile_args=['-msse2'], extra_link_args=[])
 
 
-setup ( name = "bitunpack", version = 1.0, description = "Fast bit unpacking functions", ext_modules = [module1])
+setup ( name = "bitunpack", version = "1.0", description = "Fast bit unpacking functions", ext_modules = [module1])
 
 
