16465b87e285946259c070d324e90bc23c2da496
Andrew Baldwin <baldand-bb@dehabit.info>
2014-09-02 10:07:23 +0300

    1.3.1 LUT fix1.3.1 LUT fix1.3.1 LUT fix

--- makewin.bat
+++ makewin.bat
@@ -2,6 +2,6 @@ pyinstaller --onedir dialogs.py
 copy /Y dist\dialogs\dialogs.exe .
 pyinstaller mlrawviewer_win.spec
 copy README dist\mlrawviewer\
-move dist\mlrawviewer dist\MlRawViewer_1_3_0
+move dist\mlrawviewer dist\MlRawViewer_1_3_1
 cd dist
-7z a MlRawViewer_1_3_0_win32.zip MlRawViewer_1_3_0
+7z a MlRawViewer_1_3_0_win32.zip MlRawViewer_1_3_1
