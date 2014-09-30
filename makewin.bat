aef76df417a2a38c3f5c74d434a2f9c7944d7968
Andrew Baldwin <baldand-bb@dehabit.info>
2014-09-30 23:26:02 +0300

    Add "makewin.bat" script to build Windows binary

---
+++ makewin.bat
@@ -0,0 +1,7 @@
+pyinstaller --onedir dialogs.py
+copy /Y dist\dialogs\dialogs.exe .
+pyinstaller mlrawviewer_win.spec
+copy README dist\mlrawviewer\
+move dist\mlrawviewer dist\MlRawViewer_1_3_0
+cd dist
+7z a MlRawViewer_1_3_0_win32.zip MlRawViewer_1_3_0
