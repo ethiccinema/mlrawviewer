cfeea57b764b5bb542b71514b3d04d31f0f653c9
Andrew Baldwin <baldand-bb@dehabit.info>
2014-10-04 17:48:53 +0300

    Update to 1.3.2

--- makewin.bat
+++ makewin.bat
@@ -2,6 +2,6 @@ pyinstaller --onedir dialogs.py
 copy /Y dist\dialogs\dialogs.exe .
 pyinstaller mlrawviewer_win.spec
 copy README dist\mlrawviewer\
-move dist\mlrawviewer dist\MlRawViewer_1_3_1
+move dist\mlrawviewer dist\MlRawViewer_1_3_2
 cd dist
-7z a MlRawViewer_1_3_0_win32.zip MlRawViewer_1_3_1
+7z a MlRawViewer_1_3_2_win32.zip MlRawViewer_1_3_2
